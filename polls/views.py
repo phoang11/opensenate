c6f32ca4564cd32eea57babe09db77db8a768ede
Phuoc Hoang <phuoc.hoang@novemberstudio.com>
2014-01-24 22:03:02 +0000

    tut 3, next raising 404 error

--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,19 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from polls.models import Poll
+
+def index(request):
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
